d3833d0763ff573bf7adcb847d336d0ccff9b015
Gaiish <gdishim@gmail.com>
2019-05-20 17:25:34 +0300

    added catch err on auth

--- App.js
+++ App.js
@@ -1,54 +1,63 @@
-import React, {Component} from 'react'
-import {
-  YellowBox, ActivityIndicator
-} from 'react-native'
-import firebase from 'react-native-firebase'
+import React, { Component } from "react";
+import { YellowBox, ActivityIndicator } from "react-native";
+import firebase from "react-native-firebase";
 
-import {GiftedChat} from 'react-native-gifted-chat'
+import { GiftedChat } from "react-native-gifted-chat";
 
 //react native gifted chat brings some Warnings, let's suppress them
-YellowBox.ignoreWarnings(['Warning: componentWill'])
+YellowBox.ignoreWarnings(["Warning: componentWill"]);
 
-class App extends Component{
+class App extends Component {
   state = {
     isAuthenticated: false,
     messages: []
   };
 
-  componentDidMount(){
+  componentDidMount() {
     //check if Authenticated and then get the messages from db
 
-    firebase.auth().signInAnonymously()
+    firebase
+      .auth()
+      .signInAnonymously()
       .then(() => {
         //get messages from db
         //order them by date of creation
-        firebase.database().ref('chats')
+        firebase
+          .database()
+          .ref("chats")
           .orderByChild("createdAt")
-          .on('child_added', (snap)=>{ //child_added listens to the new updates at the chats node
-            let data = snap.val()
+          .on("child_added", snap => {
+            //child_added listens to the new updates at the chats node
+            let data = snap.val();
 
-             this.setState(prev =>({
-               isAuthenticated: true,
-               messages: GiftedChat.append(prev.messages, data)
-             }));
-
-          })
+            this.setState(prev => ({
+              isAuthenticated: true,
+              messages: GiftedChat.append(prev.messages, data)
+            }));
+          });
+      })
+      .catch(err => {
+        alert("err:" + JSON.stringify(err));
       });
   }
 
   onSend(messages = []) {
     //send a new message to the db
-    firebase.database().ref('/chats')
-      .push(messages[0]).catch((err)=>{
-        alert('failed to send message:'+err)
-      })
+    firebase
+      .database()
+      .ref("/chats")
+      .push(messages[0])
+      .catch(err => {
+        alert("failed to send message:" + err);
+      });
   }
 
   render() {
-    let {isAuthenticated} = this.state
+    let { isAuthenticated } = this.state;
 
-    if (!isAuthenticated){//if not Authenticated, show a spinner
-      return <ActivityIndicator />
+    if (!isAuthenticated) {
+      //if not Authenticated, show a spinner
+      return <ActivityIndicator />;
     }
 
     return (
@@ -57,8 +66,9 @@ class App extends Component{
         onSend={messages => this.onSend(messages)}
         user={{
           _id: 1,
-          name:'user',
-          avatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP1675-CUSA11816_00-AV00000000000012//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100'
+          name: "user",
+          avatar:
+            "https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP1675-CUSA11816_00-AV00000000000012//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100"
         }}
       />
     );
